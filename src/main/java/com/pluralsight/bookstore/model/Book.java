d4f84b42771ec4d98ca2eff37755ee8559b9a4b3
leo897810 <099715@gmail.com>
2017-11-24 04:26:50 +1100

    Module-4 Entity

---
+++ src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,112 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Leo on 22/11/2017.
+ */
+@Entity
+public class Book {
+    @Id @GeneratedValue
+    private Long id;
+    @Column(length = 200)
+    private String title;
+    @Column(length = 1000)
+    private String description;
+    @Column(name="unit_cost")
+    private Float unitCost;
+    private String isbn;
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    private Integer nbOfPages;
+    private String imgaeUrl;
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImgaeUrl() {
+        return imgaeUrl;
+    }
+
+    public void setImgaeUrl(String imgaeUrl) {
+        this.imgaeUrl = imgaeUrl;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", nbOfPages=" + nbOfPages +
+                ", imgaeUrl='" + imgaeUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
---
+++ src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,9 @@
+package com.pluralsight.bookstore.model;
+
+/**
+ * Created by Leo on 24/11/2017.
+ */
+public enum Language {
+
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DEUTSCH, RUSSIAN
+}
